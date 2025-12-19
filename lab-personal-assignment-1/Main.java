82c5922f8b8421f5887a11f9147ee0a835f33de5
fahrezyfikry <fahrezyfikry@gmail.com>
2025-12-19 22:09:23 +0700

    Add files via upload

---
+++ lab-personal-assignment-1/Main.java
@@ -0,0 +1,14 @@
+class ArrayOperations {}
+
+class ArrayListOperations {}
+
+class Comparison {
+  ArrayOperations arrayOps = new ArrayOperations();
+  ArrayListOperations arrayListOps = new ArrayListOperations();
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Comparison comparison = new Comparison();
+  }
+}
